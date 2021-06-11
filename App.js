6061bb1c60fbf397b38948087049e721310b335a
Aaron <a-a-ron@Aarons-iMac.attlocal.net>
2021-06-11 07:23:16 -0400

    one working input

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const App = () => {
+  const [startTime, setStartTime] = useState(null)
+
+  const handleStartTime = (startTime) => {
+    setStartTime(startTime)
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Hey im app</Text>
+      <View style={styles.inputContainer}>
+        <Text style={{ marginRight: 10 }}>Clock In:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Start Time!"
+          keyboardType="number-pad"
+          value={startTime}
+          onChangeText={handleStartTime}
+        />
+        <Text style={{ marginLeft: 10 }}>PM</Text>
+      </View>
     </View>
   );
 };
@@ -14,9 +28,20 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    alignItems: 'center',
+    justifyContent: "center"
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: 350
+  },
+  input: {
+    height: 25,
+    width: 150,
+    borderWidth: 1,
+  }
+})
 
 export default App;
